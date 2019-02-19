5baa149c8267d8a24aaf9e7ffe5cf856b5c07f18
mariaVela790 <maria.vela790@gmail.com>
2019-02-19 17:06:43 -0600

    completed exercise 13

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called: ", script)
+print("The first variable is: ", first)
+print("The second variable is: ", second)
+print("The third variable is: ", third)
+
+user_input = input("Tell me something I don't know :)")
+print(user_input)
