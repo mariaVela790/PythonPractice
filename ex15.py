0cbd9ed57451a9f925200bf5cfa46c66c902c0c0
mariaVela790 <maria.vela790@gmail.com>
2019-02-19 17:38:09 -0600

    deleted second part of exercise

--- ex15.py
+++ ex15.py
@@ -8,10 +8,3 @@ txt = open(filename)
 print(f"Here's your file {filename}")
 #calls read method on the file object we recieved
 print(txt.read())
-
-print("Type the filename again")
-file_again = input("> ")
-
-txt_again = open(file_again)
-
-print(txt_again.read())
