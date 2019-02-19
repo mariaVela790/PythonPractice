cd586cf53b0d914a5a4d2fd712e6f17e00ec43f2
mariaVela790 <maria.vela790@gmail.com>
2019-02-19 15:40:36 -0600

    completed ex11 and study drills

---
+++ ex11.py
@@ -0,0 +1,28 @@
+#end=' ' tells the print to not end with a new line character
+# and instead end with the character specified
+
+# print("How old are you?", end=' ')
+# age = input("How old are you?")
+#
+# print("How tall are you?", end=' ')
+# height = input()
+#
+# print("How much do you weigh?", end=' ')
+# weight = input()
+#
+# print(f"So, you're {age} old, {height} tall and {weight} heavy")
+
+
+
+# print("What is your favorite color?", end=' ')
+# color = input()
+#
+# pets = input("How many pets do you have? ")
+#
+# print("What's your favorite movie?", end=' ')
+# movie = input()
+#
+# print(f"{color} is your fav color \n{pets} is the number of pets that you have and {movie} is your favorite movie!")
+
+number = int(input("Give me a number to print out? "))
+print(f"You typed in {number} and it is {isinstance(number, int)} that is is saved as an int")
