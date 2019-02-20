c8f277f87446c0ed61490f01988022b050cd5613
mariaVela790 <maria.vela790@gmail.com>
2019-02-19 18:01:28 -0600

    working on writing to a file using file output

--- ex15.py
+++ ex15.py
@@ -8,3 +8,11 @@ txt = open(filename)
 print(f"Here's your file {filename}")
 #calls read method on the file object we recieved
 print(txt.read())
+
+print("Edit the file by typing in what you want to add:")
+edit = input('> ')
+
+txt.write(f'{edit}')
+
+
+txt.close()
